772a4c197973759a0e9cedb8fc1695a7a717b4d5
Gene-3 <rhkd352@naver.com>
2025-06-24 21:54:52 +0900

    Delete SnackMall directory

--- SnackMall/order/OrderService.java
+++
@@ -1,9 +0,0 @@
-package order;
-
-import java.util.List;
-
-public interface OrderService {
-    boolean orderItems(OrderVO order);
-    List<OrderVO> listMyOrder(String memberId);
-    List<OrderVO> listAllOrder();
-}
