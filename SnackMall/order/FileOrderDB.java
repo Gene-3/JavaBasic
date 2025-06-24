5c3297b879c71d083d63da2f1a4eb73bf3632a73
Gene-3 <rhkd352@naver.com>
2025-06-24 22:00:16 +0900

    Delete SnackMall directory

--- SnackMall/order/FileOrderDB.java
+++
@@ -1,7 +0,0 @@
-package order;
-
-public interface FileOrderDB {
-    String DATA_FILE = "./data/orderDB";
-    void saveOrders();
-    void loadOrders();
-}
--- SnackMall/snack/FileSnackDB.java
+++
@@ -1,7 +0,0 @@
-package snack;
-
-public interface FileSnackDB {
-    String DATA_FILE = "./data/snackDB";
-    void saveSnacks();
-    void loadSnacks();
-}
